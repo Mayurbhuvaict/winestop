f53fe34de06040484206eb94f8aacfd2a0cdac46
tausifsunil <master_ckxftvtukc@559452.cloudwaysapps.com>
2022-07-18 06:39:15 +0000

    for captcha issue

---
+++ app/code/Winestop/Custom/Plugin/NewexpressionGenerator.php
@@ -0,0 +1,17 @@
+<?php
+namespace Winestop\Custom\Plugin;
+class NewexpressionGenerator
+{
+        public function aftergetRandomExpression(
+            \Snk\Captcha\Model\ExpressionGenerator $subject,
+            $result
+        ){
+        	if ($result['result'] == 1){
+        		$result['a'] = 10;
+        		$result['b'] = 10;
+        		$result['sign'] = '+';
+        		$result['result'] = 20;
+        	}
+        	return $result;
+        }
+}
